632c4ab8258a2b66aa4168be656b16a9b36b975e
evgenyantipin <evgeny.antipin69@gmail.com>
2026-09-22 16:14:01 +0300

    fix

--- lib/tinkoff/client.rb
+++ lib/tinkoff/client.rb
@@ -52,7 +52,7 @@ module Tinkoff
     # https://www.tinkoff.ru/kassa/develop/api/payments/sbp/qr/
     def self.get_qr(payment_id, params = {})
       params = params.merge(PaymentId: payment_id)
-      Tinkoff::Request.new('/v2/GetQR', params).perform
+      Tinkoff::Request.new('/v2/GetQr', params).perform
     end
   end
 end
